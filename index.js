f5f52cd5d2b706ce7683fc4371bdc037c4d205fc
Sinovuyo <cnovuyo10@gmail.com>
2022-05-23 20:31:46 +0200

    added on the alpine.ja, index.html,index.js files

--- index.js
+++ index.js
@@ -5,18 +5,42 @@ const assert = require('assert');
 const fs = require('fs');
 require('dotenv').config()
 
-const API = require('../_api-with-psql-workshop-with-alpineJS/api');
+
+
+const API = require('../_api-with-psql-alpineJS/api');
 const { default: axios } = require('axios');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const pgp = PgPromise({});
 const db = pgp(DATABASE_URL);
 
 API(app, db);
+
+
+
+function filterData() {
+	axios
+		.get(`/api/garments?gender=${genderFilter}&season=${seasonFilter}`)
+		.then(function(result) {
+			this.garments =  result.data.garments
+
+		});
+
+		axios
+		.get(`/api/garments`)
+		.then(function(result) {
+			this.garments =  result.data.garments
+		});
+}
+
+
+
+
 
 const PORT = process.env.PORT || 4017;
 
--- public/alpine.js
+++ public/alpine.js
@@ -1,14 +1,38 @@
+
 document.addEventListener('alpine:init', () => {
-  alert("alpine")
+  
+  
 Alpine.data('clothing', () => ({
     garments : [],
+    seasonFilter : '',
+     genderFilter : '',
+    maxPrice: 0,
     init() {
 		fetch('/api/garments')
 			.then(r => r.json())
-			.then(userData => {this.garments = userData })
-      console.log(userData);
+			.then(userData => {this.garments = userData.data })
     },
 	
+    filterData() {
+      fetch(`/api/garments?gender=${this.genderFilter}&season=${this.seasonFilter}`)
+      .then(r => r.json())
+      .then(results=>{
+          this.garments = results.data   
+      })
+}, 
+
+
+// priceRangeElem.addEventListener('change', function(evt){
+// 	const maxPrice = evt.target.value;
+// 	// showPriceRangeElem.innerHTML = maxPrice;
+
+//   fetch(`/api/garments/price/${this.maxPrice}`)
+// 		.then(function(result) {
+// 			this.garments =  result.data
+// 		});
+	  
+// 	 })
 }));
 
+	
 })

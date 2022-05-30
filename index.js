97e6edf04b9a11b10c97d88ac1b346d51fd84160
Sinovuyo <cnovuyo10@gmail.com>
2022-05-30 11:48:23 +0200

    fixed filter all in index.html

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
-const PgPromise = require("pg-promise")
-const express = require('express');
-const fs = require('fs');
-require('dotenv').config()
+const PgPromise = require("pg-promise");
+const express = require("express");
+const fs = require("fs");
+require("dotenv").config();
 
-
-
-const API = require('./api');
+const API = require("./api");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const DATABASE_URL = process.env.DATABASE_URL;
 // const PORT= process.env.PORT;
 const pgp = PgPromise({});
 // const db = pgp(DATABASE_URL);
 const config = {
-	connectionString: process.env.DATABASE_URL || 'postgres://sinovuyo:gar123@localhost:5432/garment_app',
-	max: 30,
-	ssl:{ rejectUnauthorized : false}
- };
- 
- const db = pgp(config);
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgres://sinovuyo:gar123@localhost:5432/garment_app",
+  max: 30,
+  ssl: { rejectUnauthorized: false },
+};
+
+const db = pgp(config);
 
 API(app, db);
 
-
-
 function filterData() {
-	fetch(`/api/garments?gender=${genderFilter}&season=${seasonFilter}`)
-		.then(function (result) {
-			this.garments = result.data.garments
+  fetch(`/api/garments?gender=${genderFilter}&season=${seasonFilter}`).then(
+    function (result) {
+      this.garments = result.data.garments;
+    }
+  );
 
-		});
-
-	fetch(`/api/garments`)
-		.then(function (result) {
-			this.garments = result.data.garments
-		});
+  fetch(`/api/garments`).then(function (result) {
+    this.garments = result.data.garments;
+  });
 }
-
 
 const PORT = process.env.PORT || 4017;
 
 // API routes to be added here
 
 app.listen(PORT, function () {
-	console.log(`App started on port ${PORT}`)
+  console.log(`App started on port ${PORT}`);
 });
--- public/alpine.js
+++ public/alpine.js
@@ -1,56 +1,54 @@
-
-document.addEventListener('alpine:init', () => {
-
-
-  Alpine.data('clothing', () => ({
+document.addEventListener("alpine:init", () => {
+  Alpine.data("clothing", () => ({
     garments: [],
-    seasonFilter: '',
-    genderFilter: '',
+    seasonFilter: "",
+    genderFilter: "",
     maxPrice: 0,
     garmentData: {
-      description: '',
-      price: '',
-      img: '',
-      season: '',
-      gender: '',
+      description: "",
+      price: "",
+      img: "",
+      season: "",
+      gender: "",
     },
     init() {
-      fetch('/api/garments')
-        .then(r => r.json())
-        .then(userData => { this.garments = userData.data })
+      fetch("/api/garments")
+        .then((r) => r.json())
+        .then((userData) => {
+          this.garments = userData.data;
+        });
     },
 
     filterData() {
-      fetch(`/api/garments?gender=${this.genderFilter}&season=${this.seasonFilter}`)
-        .then(r => r.json())
-        .then(results => {
-          this.garments = results.data
-        })
+      fetch(
+        `/api/garments?gender=${this.genderFilter}&season=${this.seasonFilter}`
+      )
+        .then((r) => r.json())
+        .then((results) => {
+          this.garments = results.data;
+        });
     },
 
     filterPrice() {
       fetch(`/api/garments/${this.maxPrice}`)
-        .then(r => r.json())
+        .then((r) => r.json())
         .then((result) => {
-          this.garments = result.data
+          this.garments = result.data;
         });
     },
 
-
     addGarment() {
-      console.log(this.garmentData)
-      fetch(`/api/garment/`,{
+      console.log(this.garmentData);
+      fetch(`/api/garment/`, {
         method: "POST",
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(this.garmentData)
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(this.garmentData),
       })
-        .then(r => r.json())
+        .then((r) => r.json())
         .then((result) => {
-          this.init()
+          this.init();
         })
-        .catch(err => console.log(err))
-    }
-
+        .catch((err) => console.log(err));
+    },
   }));
-
-})
+});
